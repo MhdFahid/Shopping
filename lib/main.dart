f1e29b497aecb43f118bdd1cb136ce1a8f3e033e
MhdFahid <fahidfahi786@gmail.com>
2024-07-21 13:47:04 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,22 @@
-import 'package:aldarmobile/screens/Home_screen/Home_screen.dart';
-import 'package:aldarmobile/splashScreen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
       ),
-      home: MobileLoginScreen(),
-      // home: MyApp2(),
-
     );
   }
 }
